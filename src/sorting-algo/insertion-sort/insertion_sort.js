3f11ba1f8657e2e9ede7dc827b95ab1c3165adb0
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-09-22 11:14:40 -0400

    add merge-sort directory and solution

---
+++ src/sorting-algo/insertion-sort/insertion_sort.js
@@ -0,0 +1,16 @@
+function insertionSort(arr) {
+    var currentVal;
+    for (var i = 1; i < arr.length; i++) {
+        currentVal = arr[i];
+        for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+            arr[j + 1] = arr[j]
+            console.log(`arr[${j + 1}] = arr[${j}]; ${arr[j + 1]} = ${arr[j]}`)
+        }
+        arr[j + 1] = currentVal;
+        console.log(`idx${j}+1 = currentVal ${currentVal}`)
+        console.log(arr)
+    }
+    return arr;
+}
+
+insertionSort([2, 1, 9, 76, 4])
