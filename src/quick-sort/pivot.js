a9e1cc064b6a0c1a29692c6538737bb815cab147
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-09-22 18:31:27 -0400

    add comments to pivot function for quick sort

--- src/quick-sort/pivot.js
+++ src/quick-sort/pivot.js
@@ -11,10 +11,11 @@ function pivot(arr, start = 0, end = arr.length + 1) {
 
   for (var i = start + 1; i < arr.length; i++) {
     if (pivot > arr[i]) {
-      swapIdx++;
-      swap(arr, swapIdx, i);
+      swapIdx++; // +1 ele that is less than pivot
+      swap(arr, swapIdx, i); // swap arr[i] with arr[swapIdx]
     }
   }
+  // Swap starting ele (pivot) with the pivot index
   swap(arr, start, swapIdx);
   return swapIdx;
 }
@@ -41,6 +42,15 @@ function pivot(arr, start = 0, end = arr.length - 1) {
   return swapIdx;
 }
 
-pivot([4, 8, 2, 1, 5, 7, 6, 3])
+pivot([4, 8, 2, 1, 5, 7, 6, 3]) // pivot = 4
+// move things over to left, swap them every time < 4
+// left 4 at the 1st position until very end to swap 4 to correct spot
+// pivot to return 3 at the end
+// [4,8<->2 = 2, 8, 1, 5, 7 , 6, 3]
+// [4, 2, 8<->1 = 1, 1-> 8, 5, 7 , 6, 3]
+// swapIdx = 2 + 1:
+// [4, 2, 1, 8-> 3, 5, 7, 6, 3->8]
+// swap ele at swapIdx with arr[start]
+// [4->3,2,1,3->4,5,7,6,8] // swapIdx = 3
 
 
