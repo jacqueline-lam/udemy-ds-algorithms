572333b093506cf211e21ff75eea7d730618fba6
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-09-22 11:26:45 -0400

    add comments for merge array function

--- src/merge-sort/mergeArrays.js
+++ src/merge-sort/mergeArrays.js
@@ -1,10 +1,12 @@
 // Merges two already sorted arrays
-function merge(arr1, arr2){
+function merge(arr1, arr2) {
     let results = [];
     let i = 0;
     let j = 0;
-    while(i < arr1.length && j < arr2.length){
-        if(arr2[j] > arr1[i]){
+    // while there is still data for us to look at in both arrays
+    // falsey if we hit the end of one of the arrays
+    while (i < arr1.length && j < arr2.length) {
+        if (arr2[j] > arr1[i]) {
             results.push(arr1[i]);
             i++;
         } else {
@@ -12,16 +14,19 @@ function merge(arr1, arr2){
             j++;
         }
     }
-    while(i < arr1.length) {
+    // hit end of one of the arrays
+    // add remaining items in arr #1 into results
+    while (i < arr1.length) {
         results.push(arr1[i])
         i++;
     }
-    while(j < arr2.length) {
+
+    // or add remaining items in arr #2 into results
+    while (j < arr2.length) {
         results.push(arr2[j])
         j++;
     }
     return results;
 }
-merge([100,200], [1,2,3,5,6])
+merge([100, 200], [1, 2, 3, 5, 6])
 
-                   
