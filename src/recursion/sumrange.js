bebbbe28d10a9b44dc28d299afb474947a7d5fd6
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-10-13 18:56:12 -0400

    add comments on callstack order for sumRange function

--- src/recursion/sumrange.js
+++ src/recursion/sumrange.js
@@ -1,10 +1,17 @@
-function sumRange(num){
-   if(num === 1) return 1; 
-   return num + sumRange(num-1);
+// 2nd recursive function
+function sumRange(num) {
+   if (num === 1) return 1;
+   return num + sumRange(num - 1);
 }
 
 sumRange(4)
+// return 4 + sumRange(3)
+// return 4 + 3 + sumRange(2)
+// return 4 + 3 + 2 + sumRange(1)
+// return 4 + 3 + 2 + 1 // walk thru callstack
+// return 4 + 3 + 3
+// return 4 + 6
+// return 10
 
-                  
-                                   
-                                
+
+
