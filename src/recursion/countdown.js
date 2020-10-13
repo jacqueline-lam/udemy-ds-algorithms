3b98a047ea72b1ccb43fc0f4ee798a1ea685de7b
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-10-13 17:49:50 -0400

    add recursion comments for countDown function

--- src/recursion/countdown.js
+++ src/recursion/countdown.js
@@ -1,6 +1,6 @@
 // Recursive Version
-function countDown(num){
-    if(num <= 0) {
+function countDown(num) {
+    if (num <= 0) {
         console.log("All done!");
         return;
     }
@@ -9,12 +9,22 @@ function countDown(num){
     countDown(num);
 }
 countDown(3)
+// print 3
+// countDown(2)
+// print 2
+// countDown(1)
+// print 1
+// countDown(0)
+// 'All Done!'
+// return nothing - BASE CASE
 
 // Iterative Version
-function countDown(num){
-    for(var i = num; i > 0; i--){
+function countDown(num) {
+    for (var i = num; i > 0; i--) {
         console.log(i);
     }
     console.log("All done!")
 }
+
+
 
