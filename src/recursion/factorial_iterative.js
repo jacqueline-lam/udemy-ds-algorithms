be447964fd5268f7010d99d318b804be87c55bdd
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-10-13 19:06:20 -0400

    add comments to fexlpain actorial iteractive and recursive functions

--- src/recursion/factorial_iterative.js
+++ src/recursion/factorial_iterative.js
@@ -1,7 +1,11 @@
-function factorial(num){
+function factorial(num) {
     let total = 1;
-    for(let i = num; i > 1; i--){
+    // no reason to set condition to be i > 0 because no need to *1
+    for (let i = num; i > 1; i--) {
         total *= i
     }
     return total;
 }
+
+factorial(3) // 3 * 2 = 6
+factorial(4) // 4 * 3 * 2 = 24
--- src/recursion/factorial_recursive.js
+++ src/recursion/factorial_recursive.js
@@ -1,4 +1,10 @@
-function factorial(num){
-    if(num === 1) return 1;
-    return num * factorial(num-1);
+function factorial(num) {
+    if (num === 1) return 1; // BASE CASE
+    return num * factorial(num - 1);
 }
+
+factorial(4)
+// return 4 * factorial(3)
+// return 4 * 3 * factorial(2)
+// return 4 * 3 * 2 * factorial(1)
+// return (4 * (3 * (2 * 1))) // 24
