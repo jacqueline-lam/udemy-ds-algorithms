4ee9a214a0cbf43e13a4ae7f3c0008eda8cbd92b
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-10-13 20:14:03 -0400

    add Helper Method Recursion Fn to collect odd values and comments to explain fn

---
+++ src/recursion/helper_method_recursion.js
@@ -0,0 +1,22 @@
+function collectOddValues(arr) { //outer fn - NOT recursive
+
+    let result = []; // use helper method to prevent resetting arr to empty
+
+    function helper(helperInput) {  // inner fn - recursive
+        if (helperInput.length === 0) {
+            return;
+        }
+
+        if (helperInput[0] % 2 !== 0) {
+            result.push(helperInput[0])
+        }
+
+        helper(helperInput.slice(1))
+    }
+
+    helper(arr)
+
+    return result;
+}
+
+collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9])
