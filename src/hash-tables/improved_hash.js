eca5ca744a273572c2c7cef13cc0956e06c4211c
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-10-16 17:37:48 -0400

    add comments on how  the imporved hash function makes use of prime number to decrease collisions

--- src/hash-tables/improved_hash.js
+++ src/hash-tables/improved_hash.js
@@ -1,6 +1,9 @@
+// use prime number for arrayLen  and * weird prime to
+// decrease # of collisions to have more distributed data
 function hash(key, arrayLen) {
   let total = 0;
   let WEIRD_PRIME = 31;
+  // loop at most 100 times
   for (let i = 0; i < Math.min(key.length, 100); i++) {
     let char = key[i];
     let value = char.charCodeAt(0) - 96
