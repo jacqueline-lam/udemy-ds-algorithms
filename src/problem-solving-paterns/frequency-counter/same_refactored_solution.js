14f4fad713631d9de1a1f867447e29a49f49dfff
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-10-21 20:04:20 -0400

    add refactored frequncy counter solution and comments

---
+++ src/problem-solving-paterns/frequency-counter/same_refactored_solution.js
@@ -0,0 +1,31 @@
+// FREQUENCY COUNTER REFACTORED SOLUTIOn
+function same(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    let frequencyCounter1 = {}
+    let frequencyCounter2 = {}
+    for (let val of arr1) {
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+    }
+    for (let val of arr2) {
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+    }
+    console.log(frequencyCounter1);
+    console.log(frequencyCounter2);
+
+    // in operator returns true if  specified property is in specified object
+    for (let key in frequencyCounter1) {
+        // check if the key ** 2 is a key in second object i.e. key 4 exists?
+        if (!(key ** 2 in frequencyCounter2)) {
+            return false
+        }
+        // check if the key occurs same amount of times - i.e. two 4?
+        if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+            return false
+        }
+    }
+    return true
+}
+
+same([1, 2, 3, 2, 5], [9, 1, 4, 4, 11])
