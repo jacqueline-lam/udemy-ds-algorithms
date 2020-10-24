a59b20a5ada64c5a16e69525941ec1ad355c8a68
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-10-23 22:02:35 -0400

    add sameFrequency fn to fevaluate if 2 numbers have same frequency of digits

---
+++ src/problem-solving-paterns/frequency-counter/same_frequency.js
@@ -0,0 +1,53 @@
+// given 2 +ve integers, find out if the 2 numbers have same frequency of digits
+function sameFrequency(num1, num2) {
+  let strNum1 = num1.toString();
+  let strNum2 = num2.toString();
+  if (strNum1.length !== strNum2.length) return false;
+
+  let countNum1 = {};
+  let countNum2 = {};
+
+  for (let i = 0; i < strNum1.length; i++) {
+    countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1
+  }
+
+  for (let j = 0; j < strNum1.length; j++) {
+    countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1
+  }
+
+  for (let key in countNum1) {
+    if (countNum1[key] !== countNum2[key]) return false;
+  }
+
+  return true;
+}
+
+
+// MY SOLUTION
+function sameFrequency(num1, num2) {
+  let strNum1 = num1.toString();
+  let strNum2 = num2.toString();
+  if (strNum1.length !== strNum2.length) return false;
+
+  let countNum1 = {};
+  let countNum2 = {};
+
+  for (let val of strNum1) {
+    countNum1[val] = (countNum1[val] || 0) + 1
+  }
+
+  for (let val of strNum2) {
+    countNum2[val] = (countNum2[val] || 0) + 1
+  }
+
+  for (let key in countNum2) {
+    if (countNum1[key] !== countNum2[key]) return false
+  }
+
+  return true;
+}
+
+sameFrequency(181, 281) // true
+sameFrequency(34, 14) // false
+sameFrequency(3589578, 5879385) // true
+sameFrequency(22, 222) // false
