9449ea2e08c7915680f50a685450f49c7e062edc
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-10-27 14:14:15 -0400

    add sliding window solution to minSubArrayLen fn and add comments to explain approach

--- src/problem-solving-paterns/sliding-window/min-subarray-len.js
+++ src/problem-solving-paterns/sliding-window/min-subarray-len.js
@@ -3,37 +3,41 @@
 // if there isn't one, return 0
 // O(N) Time Complexity and O(1) Space Complexity
 
-function minSubArrayLen(array, integer) {
-  let tempLength = 0;
-  let minLength = 0;
-}
-
-// solution
 function minSubArrayLen(nums, sum) {
   let total = 0;
   let start = 0;
   let end = 0;
-  let minLen = Infinity;
+  let minLength = Infinity;
 
   while (start < nums.length) {
     // if current window doesn't add up to the given sum then
-    // move the window to right
     if (total < sum && end < nums.length) {
+      // move the window to RIGHT
       total += nums[end];
       end++;
-    }
-    // if current window adds up to at least the sum given then
-    // we can shrink the window
-    else if (total >= sum) {
-      minLen = Math.min(minLen, end - start);
+      // if current window adds up to at least the sum given then
+    } else if (total >= sum) {
+      minLength = Math.min(minLength, end - start);
+      // we can shrink the window
+      // remove first ele of current window
       total -= nums[start];
       start++;
-    }
-    // current total less than required total but we reach the end, need this or else we'll be in an infinite loop
-    else {
+    } else {
+      // current total < required total but we reach the end,
+      // need this or else we'll be in an infinite loop
       break;
     }
   }
-
-  return minLen === Infinity ? 0 : minLen;
+  return minLength === Infinity ? 0 : minLength;
 }
+
+// Approach: Move the starting idx of current subarray as soon as
+// no better could be done with this idx as the starting idx
+/// We could keep 2 pointer, one for the start and another for the end of the current subarray
+// and make optimal moves so as to keep the total ? sum
+// & maintain the lowest size possible
+
+minSubArrayLen([2, 3, 1, 2, 4, 3], 7) //2
+minSubArrayLen([2, 1, 6, 5, 4], 9) //2
+minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52) //1
+minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95) //0
