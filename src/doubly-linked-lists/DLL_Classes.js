f5ac87671a7bd82a2d1bed6ecd4a6d903f8b784c
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-11-05 01:42:32 -0500

    create Doubly Linked List class for all DLL operations - add push method

--- src/doubly-linked-lists/DLL_Classes.js
+++ src/doubly-linked-lists/DLL_Classes.js
@@ -1,13 +1,22 @@
-class Node{
-    constructor(val){
+// Node
+// - val
+// -next
+// -prev
+class Node {
+    constructor(val) {
         this.val = val;
         this.next = null;
         this.prev = null;
     }
 }
 
-class DoublyLinkedList{
-    constructor(){
+// Doubly Linked List
+// -head
+// -tail
+// -length
+
+class DoublyLinkedList {
+    constructor() {
         this.head = null;
         this.tail = null;
         this.length = 0;
---
+++ src/doubly-linked-lists/Doubly-Linked-List.js
@@ -0,0 +1,34 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    // add node to end of DLL
+    push(val) {
+        let newNode = new Node(val);
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
+
+let list = new DoubleLinkedList
+list.push('first item')
+list.push('last item')
