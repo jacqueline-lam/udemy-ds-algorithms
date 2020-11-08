9ae96cfc5a28a842a23c2dd69f8e0061ba976b14
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-11-07 23:58:41 -0500

    add unshift method to DoubleLinkedList to add node to beginning of DLL

--- src/doubly-linked-lists/Doubly-Linked-List.js
+++ src/doubly-linked-lists/Doubly-Linked-List.js
@@ -64,7 +64,20 @@ class DoublyLinkedList {
         this.length--;
         return oldHead; // removed node
     }
-}
+
+    unshift(val) {
+        var newNode = new Node(val);
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this
+    }
 
 let list = new DoubleLinkedList
 list.push('first item')
