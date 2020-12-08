e35a51bb0e01a7cbc08454d5897ac2323119351f
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-12-08 18:55:01 -0500

    add queue class and enqueue and dequeue methods to demonstrate FIFO principle

--- src/stack_and_queue/queue.js
+++ src/stack_and_queue/queue.js
@@ -14,4 +14,40 @@ queue // ['third', 'second', 'first']
 queue.pop() //'first'
 
 // Implementation with SLL - to achieve constant time for addition/ removal
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  enqueue(val) { // add something into queue
+    var newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+
+  dequeue() { // return & remove first thing added in
+    if (!this.first) return null;
+
+    var temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
