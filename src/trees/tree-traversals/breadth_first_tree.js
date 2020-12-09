b5b48dfc215ef0af03a5046bafb59552cc73f9fd
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-12-09 15:13:02 -0500

    add insert, find, and contains methods to BST class

--- src/trees/tree-traversals/breadth_first_tree.js
+++ src/trees/tree-traversals/breadth_first_tree.js
@@ -11,10 +11,67 @@ class BinarySearchTree {
     this.root = null;
   }
 
+  insert(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+  find(value) {
+    if (this.root === null) return false;
+    let current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return undefined;
+    return current;
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    let current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+
+
 }
 
 
-var tree = new BinarySearchTree();
+let tree = new BinarySearchTree();
 tree.insert(10);
 tree.insert(6);
 tree.insert(15);
