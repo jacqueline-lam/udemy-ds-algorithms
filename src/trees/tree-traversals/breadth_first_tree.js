2f47c2b03d967c89a7115d8cf02af8437daf570e
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-12-09 15:05:39 -0500

    add barebone of node  and BFS class

---
+++ src/trees/tree-traversals/breadth_first_tree.js
@@ -0,0 +1,26 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+}
+
+
+var tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+tree.BFS();
+
+
