078d5ea55cad1ac89bc1491bd3469e1964968e72
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-12-10 17:18:05 -0500

    add DFSInOrder tmethod o traverse entire left, visit node, and then traverse right

--- src/trees/tree-traversals/depth_first_tree.js
+++ src/trees/tree-traversals/depth_first_tree.js
@@ -109,6 +109,20 @@ class BinarySearchTree {
 
     return data;
   }
+
+  DFSInOrder() {
+    let data = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      // does same thing to check if node.left is truthy
+      // node.left && traver(node.left);
+      data.push(node);
+      if (node.right) traverse(node.right);
+    }
+    traverse(node.value);
+    return data;
+  }
 }
 
 let tree = new BinarySearchTree();
@@ -120,3 +134,4 @@ tree.insert(8);
 tree.insert(20);
 tree.DFSPreOrder(); // [10, 6, 3, 8, 15, 20]
 tree.DFSPostOrder(); // [3, 8, 6, 20, 15, 10]
+tree.DFSInorder(); // [3, 6, 8, 10, 15, 20]
