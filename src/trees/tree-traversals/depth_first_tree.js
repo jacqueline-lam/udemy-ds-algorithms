56ae2a72c945fee7394403ec93b689db9ab9580f
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-12-10 16:46:24 -0500

    add an instance of BST for ttesting code

--- src/trees/tree-traversals/depth_first_tree.js
+++ src/trees/tree-traversals/depth_first_tree.js
@@ -97,3 +97,12 @@ class BinarySearchTree {
     return data; // return order of visiting nodes
   }
 }
+
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+tree.DFSPreOrder(); // [10, 6, 3, 8, 15, 20]
