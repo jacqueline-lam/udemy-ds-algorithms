b70e58d15c9d677a68c7c94b64eaf781859c5b90
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-12-27 23:59:21 -0500

    add pseudocode to validAnagram problem

---
+++ src/algorithms/strings/valid_anagram.js
@@ -0,0 +1,31 @@
+// Given two strings s and t , write a function to determine if t is an anagram of s.
+
+// Example 1:
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+
+// Example 2:
+// Input: s = "rat", t = "car"
+// Output: false
+
+// Note:
+// You may assume the string contains only lowercase alphabets.
+// Follow up:
+// What if the inputs contain unicode characters?
+// How would you adapt your solution to such case?
+
+// Frequecy Counter Solution
+let validAnagram = function (s, t) {
+  // return false if length doesn't match
+
+  // create empty object `lookup`
+  // current obj - a break down of `s`
+  // if ltr exists, increment, otherwise, set loopup[letter]to 1
+
+  // loop over second str
+  // if can't find  or letter = 0, return false
+  // else decrement lookup[letter] <-- since ltr is found
+  // return true
+}
+
+
