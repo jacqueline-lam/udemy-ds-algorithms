9d983de5503d3702a38ab5e42441458a0c827e23
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2021-02-26 23:58:00 -0500

    add recursive solution to isValidBST

--- src/algorithms/trees/validate_BST.js
+++ src/algorithms/trees/validate_BST.js
@@ -46,5 +46,20 @@
 
 let isValidBST = function (root) {
   // edge case - check if root is null
+  // if (!root) return false;
   // use recursion to check left and right node
+  function helper(root, min, max) {
+    if (!root) {
+      return true; // We hit the end of the path
+    }
+
+    if ((min !== null && root.val <= min) || (max !== null && root.val >= max)) {
+      return false; // current node's val doesn't satisfy the BST rules
+    }
+
+    // Continue to scan left and right
+    return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+  }
+
+  return helper(root, null, null);
 };
