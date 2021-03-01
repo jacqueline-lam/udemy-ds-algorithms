e42757a4fd9c10ead7e658c57fd51c92f9b87b09
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2021-02-28 23:49:03 -0500

    add solution B to isValidBST

--- src/algorithms/trees/validate_BST.js
+++ src/algorithms/trees/validate_BST.js
@@ -62,4 +62,19 @@ let isValidBST = function (root) {
   }
 
   return helper(root, null, null);
+
+};
+
+let isValidBSTB = function (root) {
+  if (!root)
+    return true
+  return dfs(root, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)
+
+  function dfs(root, min, max) {
+    if (!root)
+      return true
+    if (root.val <= min || root.val >= max)
+      return false
+    return dfs(root.left, min, root.val) && dfs(root.right, root.val, max)
+  }
 };
