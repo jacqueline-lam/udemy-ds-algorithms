1421591e22d592ded3f89fdbfaeaffbc49eb6c75
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2021-04-14 22:53:46 -0400

    add linked list solution to addTwoNumbers

--- src/algorithms/linked-lists/add_two_numbers.js
+++ src/algorithms/linked-lists/add_two_numbers.js
@@ -43,5 +43,17 @@
 //  */
 
 let addTwoNumbers = function (l1, l2) {
-
+  // Solution example
+  const d = new ListNode();
+  let curr = d;
+  let carry = 0;
+  while (l1 || l2 || carry) {
+    const sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+    carry = sum / 10 | 0;
+    curr.next = new ListNode(sum % 10);
+    if (l1) l1 = l1.next;
+    if (l2) l2 = l2.next;
+    curr = curr.next;
+  }
+  return d.next;
 };
