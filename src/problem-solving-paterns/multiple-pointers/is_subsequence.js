da295e787f9a6129c824611b6ecbeed133f54813
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2022-02-11 23:08:45 -0500

    add iterative solution for isSubsequence to enable O(1) space solution

--- src/problem-solving-paterns/multiple-pointers/is_subsequence.js
+++ src/problem-solving-paterns/multiple-pointers/is_subsequence.js
@@ -7,7 +7,7 @@ function isSubsequence(str1, str2) {
   if (str1.length === 0) return true;
   if (str2.length === 0) return false;
 
-  // recursions
+  // recursions - Recursive but not O(1) Space
   if (str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1));
   // if char doesn't match, continue comparing char at str1 w/ next char in str2
   return isSubsequence(str1, str2.slice(1))
@@ -20,29 +20,17 @@ isSubsequence('abc', 'acb') // false (order matters)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function isSubsequence(str1, str2) {
-  if (str1.length === 0) return true
-  if (str2.length === 0) return false
-  if (str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))
-  return isSubsequence(str1, str2.slice(1))
+// Iterative Solution - O(1) Space
+// isSubsequence Solution - Iterative
+function isSubsequenceB(str1, str2) {
+  var i = 0;
+  var j = 0;
+  if (!str1) return true;
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++;
+    // No remaining char to evaluate in str1 (all chars have been matched in str2)
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
 }
