12a69bc17fe9c0514be865920d8bddf00102d5ca
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2022-04-05 17:09:05 -0400

    add solution to reverseLettersInPlace using 2-pointer approacj

---
+++ src/algorithms/interview_cake/reverse_string_in_place.js
@@ -0,0 +1,31 @@
+/*
+Write a function that takes an array of characters
+and reverses the letters in place. ↴
+
+- Why an array of characters instead of a string?
+
+- The goal of this question is to practice manipulating strings in place.
+Since we're modifying the input, we need a mutable ↴ type like an array,
+instead of JavaScript's immutable strings.
+
+Example:
+Input: ['glee','expert','boba','apple']
+Output: ['apple', 'boba','expert','glee']
+*/
+
+// 2 pointer
+function reverseLettersInPlace(arrOfChars) {
+  let start = 0;
+  let end = arrOfChars.strength - 1;
+
+  while (start < end) {
+    let temp = arrOfChars[start]
+    arrOfChars[start] = arrOfChars[end];
+    arrOfChars[end] = temp;
+    start++;
+    end--;
+  }
+  return arrOfChars;
+}
+
+// Same question as reverse_string algo>strings>reverse_strings.js
